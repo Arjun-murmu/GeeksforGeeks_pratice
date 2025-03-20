1b48a797f7476e99cfa8535b3e709a3681a2923f
Arjun Murmu <92778275+Arjun-murmu@users.noreply.github.com>
2025-03-21 03:10:05 +0530

    Create +ve_and_-ve_value.js

---
+++ JavaScript/+ve_and_-ve_value.js
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(; i<t; i++) {
+        let a = parseInt(readLine());
+        let obj = new Solution();
+        obj.utility(a);
+    
+console.log("~");
+}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+class Solution {
+    utility(number) {
+        // write your code here
+        // if(number > 0){
+        //     return "Positive"
+        // }
+        // if(number < 0){
+        //     return "Negative"
+        // }
+        // else{
+        //     return "Zero"
+        // }
+        console.log(
+            number > 0 ? "Positive" :
+            number < 0 ? "Negative" : "Zero"
+        );
+    }
+}
